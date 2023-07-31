359621340b42d770f851caac6deb16fe0e4dd40e
anasmuostafa <anasmuostafa@gmail.com>
2023-07-31 08:24:03 +0300

    starting the project setupt to use webpack

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+'use strict'
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    /* keep only one file of js in dist instead of creating one after every change */
+    clean: true,
+    /* so that the image names stay the same after compiling */
+    assetModuleFilename: "[name][ext]",
+  },
+  devtool: "source-map",
+  /* creating developer server */
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  /* module for adding the loaders init*/
+  module: {
+    rules: [
+      {
+        test: /\.(css)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  autoprefixer
+                ]
+              }
+            }
+          },
+        ]
+      },
+      {
+        test: /\.(svg|jpeg|jpg|png|gif)$/i,
+        type: "asset/resource",
+      }]},
+  
+  /* array of plugins */
+  plugins: [
+    /* html plugin to create the in the src instead of the dist*/
+    new HtmlWebpackPlugin({
+      title: "webpack App",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+    new BundleAnalyzerPlugin(), 
+  ],
+};
