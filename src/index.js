1f1980dabf3493ee656d336df25199b03706ea04
anasmuostafa <anasmuostafa@gmail.com>
2023-08-05 14:52:25 +0300

    change img and font colors with tempreture change

--- src/index.js
+++ src/index.js
@@ -2,31 +2,36 @@ import { main } from "./styles/main.css";
 import { weatherToday } from "./script/weather";
 import { fixedQuery } from "./script/fixedQuery";
 import { Change } from "./script/weatherdom";
-import { loading, appendValuesToDom } from "./script/dom";
+import {
+  loading,
+  appendValuesToDom,
+  changeToC,
+  changeToF,
+  changeBackground,
+  changeStyleColor,
+} from "./script/dom";
+
+
 
 /* search a query */
 const search = document.getElementById("search");
 search.addEventListener("change", () => {
   let query = search.value;
-  console.log(query);
   searchThenFetch(query);
   loading();
 });
 document.querySelector("i.search").addEventListener("click", () => {
   let query = search.value;
-  console.log(query);
   searchThenFetch(query);
   loading();
 });
 
 /* search the input query and select the first in result searched array */
 const searchThenFetch = (query) => {
-  console.log(query);
   fixedQuery(query).then(fetchThenLoad);
 };
 
 function fetchThenLoad(fixedQuery) {
-  console.log(fixedQuery);
   fetch(
     `http://api.weatherapi.com/v1/forecast.json?key=e28ec3b945254a078d871023232807&q=${fixedQuery}&days=3`,
     { mode: "cors" }
@@ -38,6 +43,11 @@ function fetchThenLoad(fixedQuery) {
       console.log(resp);
       const today = new weatherToday(resp);
       const changeDom = new Change();
+      console.log(today.hour__);
+      console.log(getWeatherStatusNumber(today));
+      let state = getWeatherStatusNumber(today);
+      changeBackground(state);
+      changeStyleColor(state);
       /* append weather data */
       let measureType = document.querySelector(".btn.selected").id;
       appendValuesToDom(changeDom, today, measureType);
@@ -45,10 +55,37 @@ function fetchThenLoad(fixedQuery) {
         btn.addEventListener("click", () => {
           if (!btn.classList.contains("selected")) {
             const selected = document.querySelector(".selected");
-            toggle(btn);
-            toggle(selected);
+            btn.classList.add("selected");
+            selected.classList.toggle("selected");
+            changeWeatherMeasure(btn.id, changeDom);
           }
         });
       });
     });
 }
+/* const changeWeatherMeasure = (key, changeDom, todayData) => {
+  if (key == "f") changeToF(changeDom, todayData);
+  else if (key == "c") changeToC(changeDom, todayData);
+}; */
+
+const changeWeatherMeasure = (key, changeDom) => {
+  if (key == "f") changeToF(changeDom);
+  else if (key == "c") changeToC(changeDom);
+};
+const getWeatherStatusNumber = (weatherData) => {
+  if (weatherData.humidity__ >= 90) return "rain";
+  if (weatherData.cloud__ <= 25) {
+    if (weatherData.hour__ >= 6 && weatherData.hour__ <= 18) {
+      return "sunny";
+    } else {
+      return "clear";
+    }
+  } else if (weatherData.cloud__ <= 75) {
+    return "partlycloudy";
+  } else return "cloudy";
+};
+
+
+/* search for alexandria egypt as start */
+loading();
+searchThenFetch('paris');
--- src/script/weather.js
+++ src/script/weather.js
@@ -1,46 +1,61 @@
 class weatherToday {
-    constructor(resp){
-        this.resp = resp;
-    }
-    get fullName(){
-        return `${this.resp.location.name}, ${this.resp.location.country}`;
-    }
-    get date(){
-        return `${this.resp.location.localtime.split(' ')[0]}`;
-    }
-    get time(){
-        return `${this.resp.location.localtime.split(' ')[1]}`;
-    }
-    get temp_c(){
-        return `${this.resp.current.temp_c} °C`;
-    }
-    get temp_f(){
-        return `${this.resp.current.temp_f} °F`;
-    }
-    get condition(){
-        return `${this.resp.current.condition.text}`;
-    }
-    get temp_feelLike_c(){
-        return `feels like  ${this.resp.current.feelslike_c} °C`;
-    }
-    get temp_feelLike_f(){
-        return `feelsl like ${this.resp.current.feelslike_f} °F`;
-    }
-    get iconUrl(){
-        return `${this.resp.current.condition.icon}`;
-    }
-    get wspeed_kph(){
-        return `${this.resp.current.wind_kph} kph`
-    }
-    get wspeed_mph(){
-        return `${this.resp.current.wind_kph} mph`
-    }
-    get humidity(){
-        return `${this.resp.current.humidity} %`;
-    }
-    get rain(){
-        return `${this.resp.forecast.forecastday[0].hour[0].chance_of_rain} %`;
-    }
+  constructor(resp) {
+    this.name__ = resp.location.name;
+    this.country__ = resp.location.country;
+    this.date__ = resp.location.localtime.split(" ")[0];
+    this.time__ = resp.location.localtime.split(" ")[1];
+    this.hour__ = this.time.split(":")[0]; 
+    this.temp_c__ = resp.current.temp_c;
+    this.temp_f__ = resp.current.temp_f;
+    this.condition__ = resp.current.condition.text;
+    this.temp_feelLike_c__ = resp.current.feelslike_c;
+    this.temp_feelLike_f__ = resp.current.feelslike_f;
+    this.iconUrl__ = resp.current.condition.icon;
+    this.wspeed_kph__ = resp.current.wind_kph;
+    this.wspeed_mph__ = resp.current.wind_mph;
+    this.humidity__ = resp.current.humidity;
+    this.rain__ = resp.forecast.forecastday[0].hour[0].chance_of_rain;
+    this.cloud__ = resp.current.cloud; 
+  }
+  get fullName() {
+    return `${this.name__}, ${this.country__}`;
+  }
+  get date() {
+    return `${this.date__}`;
+  }
+  get time() {
+    return `${this.time__}`;
+  }
+  get temp_c() {
+    return `${this.temp_c__} °C`;
+  }
+  get temp_f() {
+    return `${this.temp_f__} °F`;
+  }
+  get condition() {
+    return `${this.condition__}`;
+  }
+  get temp_feelLike_c() {
+    return `feels like ${this.temp_feelLike_c__} °C`;
+  }
+  get temp_feelLike_f() {
+    return `feels like ${this.temp_feelLike_f__} °F`;
+  }
+  get iconUrl() {
+    return `${this.iconUrl__}`;
+  }
+  get wspeed_kph() {
+    return `${this.wspeed_kph__} km/h`;
+  }
+  get wspeed_mph() {
+    return `${this.wspeed_mph__} mi/h`;
+  }
+  get humidity() {
+    return `${this.humidity__} %`;
+  }
+  get rain() {
+    return `${this.rain__} %`;
+  }
 }
 
 export { weatherToday };
