21e470e55f0c8f1b3fb67ce3efc063c0e2bef87b
anasmuostafa <anasmuostafa@gmail.com>
2023-08-14 03:29:55 +0300

    minimize request numbers for managing free plan

--- src/script/fetchCoordinates.js
+++ src/script/fetchCoordinates.js
@@ -1,7 +1,7 @@
 const fetchCoordinates = () => {
   return new Promise((resolve, reject) => {
     if ("geolocation" in navigator) {
-      const id = navigator.geolocation.watchPosition((position) => {
+      const id = navigator.geolocation.getCurrentPosition((position) => {
         console.log(position.coords.latitude);
         resolve({
           lat: parseFloat(position.coords.latitude),
